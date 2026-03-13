943cd5e16811c5dfd66af1a96f11ab1d56e123d7
aydocs <cihanayberk60@gmail.com>
2026-03-13 12:01:56 +0300

    pos: update pos/vite.config.ts

--- pos/vite.config.ts
+++ pos/vite.config.ts
@@ -10,17 +10,14 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 export default defineConfig({
   server: {
     proxy: {
-      '/storage': {
-        target: 'https://rezervist.com',
+      '/api': {
+        target: 'http://localhost:8000',
         changeOrigin: true,
-        secure: false, // In case of SSL issues
+        secure: false,
         headers: {
-          'Host': 'rezervist.com',
-          'Origin': 'https://rezervist.com',
-          'Referer': 'https://rezervist.com/',
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-          'Accept-Language': 'en-US,en;q=0.9,tr-TR;q=0.8,tr;q=0.7'
+          'Host': 'localhost:8000',
+          'Origin': 'http://localhost:8000',
+          'Referer': 'http://localhost:8000/',
         }
       }
     }
