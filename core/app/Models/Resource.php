790063363eccb7a7f3a2c0d325fe344d11aa246f
aydocs <cihanayberk60@gmail.com>
2026-03-13 12:00:52 +0300

    model: update core/app/Models/Resource.php

--- core/app/Models/Resource.php
+++ core/app/Models/Resource.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Resource extends Model
 {
-    use HasFactory;
+    use HasFactory, \App\Traits\HasBusinessScope;
 
     protected $fillable = [
         'business_id',
