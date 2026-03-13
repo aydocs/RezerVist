f785cb1dcc3cf8cbc7bc80bab371a03d95022df9
aydocs <cihanayberk60@gmail.com>
2026-03-13 12:01:01 +0300

    update core/config/cors.php

--- core/config/cors.php
+++ core/config/cors.php
@@ -23,7 +23,7 @@ return [
         env('FRONTEND_URL', 'http://localhost:5173'),
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://rezervist.com',
+        'http://localhost:8000',
     ],
 
     'allowed_origins_patterns' => [],
